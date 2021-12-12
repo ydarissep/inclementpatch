cd287bcb650d14955cc02fe36bc70558324547cb
ydarissep <60859033+ydarissep@users.noreply.github.com>
2021-12-12 23:24:10 +0100

    Update pokenav_match_call_2.c

--- src/pokenav_match_call_2.c
+++ src/pokenav_match_call_2.c
@@ -620,6 +620,7 @@ u32 DoMatchCallMessage(s32 taskState)
 
 u32 DoTrainerCloseByMessage(s32 taskState)
 {
+    /*
     struct Pokenav4Struct *state = GetSubstructPtr(POKENAV_SUBSTRUCT_MATCH_CALL_OPEN);
     switch (taskState)
     {
@@ -640,7 +641,7 @@ u32 DoTrainerCloseByMessage(s32 taskState)
             return LT_PAUSE;
         break;
     }
-
+    */
     return LT_FINISH;
 }
 
