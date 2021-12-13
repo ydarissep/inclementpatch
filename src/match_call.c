54c9578a610ec647f3f358bb98ce401058ef1959
ydarissep <60859033+ydarissep@users.noreply.github.com>
2021-12-13 02:58:54 +0100

    Update match_call.c

--- src/match_call.c
+++ src/match_call.c
@@ -1155,8 +1155,11 @@ bool32 TryStartMatchCall(void)
         && MapAllowsMatchCall()
         && SelectMatchCallTrainer())
     {
+        /*
         StartMatchCall();
         return TRUE;
+        */
+        return FALSE;
     }
 
     return FALSE;
