b2b0454be86c5f4d5c2a5ec8785fd807bab5f452
ydarissep <60859033+ydarissep@users.noreply.github.com>
2021-12-16 22:34:46 +0100

    Update party_menu.c

--- src/party_menu.c
+++ src/party_menu.c
@@ -6087,7 +6087,7 @@ void OpenPartyMenuInBattle(u8 partyAction)
     InitPartyMenu(PARTY_MENU_TYPE_IN_BATTLE, GetPartyLayoutFromBattleType(), partyAction, FALSE, PARTY_MSG_CHOOSE_MON, Task_HandleChooseMonInput, CB2_SetUpReshowBattleScreenAfterMenu);
     ReshowBattleScreenDummy();
     UpdatePartyToBattleOrder();
-    ShowPokemonSummaryScreen(SUMMARY_MODE_LOCK_MOVES, gPlayerParty, gPartyMenu.slotId, gPlayerPartyCount - 1, CB2_ReturnToPartyMenuFromSummaryScreen);
+    ShowPokemonSummaryScreen(SUMMARY_MODE_LOCK_MOVES, gEnemyParty, gPartyMenu.slotId, gEnemyPartyCount - 1, CB2_ReturnToPartyMenuFromSummaryScreen);
 }
 
 void ChooseMonForInBattleItem(void)
