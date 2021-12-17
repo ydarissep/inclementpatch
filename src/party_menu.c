ad3ad5e05d4997472a5c140d6cd8d99ed82b8b7f
ydarissep <60859033+ydarissep@users.noreply.github.com>
2021-12-17 05:27:44 +0100

    Update party_menu.c

--- src/party_menu.c
+++ src/party_menu.c
@@ -6085,7 +6085,7 @@ static u8 GetPartyLayoutFromBattleType(void)
 
 void OpenPartyMenuInBattle(u8 partyAction)
 {
-    InitPartyMenu(PARTY_MENU_TYPE_IN_BATTLE, GetPartyLayoutFromBattleType(), partyAction, FALSE, PARTY_MSG_CHOOSE_MON, Task_HandleChooseMonInput, CB2_SetUpReshowBattleScreenAfterMenu);
+    InitPartyMenu(PARTY_MENU_TYPE_IN_BATTLE, GetPartyLayoutFromBattleType(), partyAction, FALSE, PARTY_MSG_CHOOSE_MON, Task_HandleChooseMonInput, CB2_SetUpExitToBattleScreen);
     ReshowBattleScreenDummy();
     UpdatePartyToBattleOrder();
     ShowPokemonSummaryScreen(SUMMARY_MODE_LOCK_MOVES, gEnemyParty, gPartyMenu.slotId, gEnemyPartyCount - 1, ExitPartyMenu);
