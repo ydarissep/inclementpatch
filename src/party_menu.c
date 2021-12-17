3cb11afe42e9eea1c202b649cf7cbb9f6d6d882c
ydarissep <60859033+ydarissep@users.noreply.github.com>
2021-12-17 04:44:00 +0100

    Update party_menu.c

--- src/party_menu.c
+++ src/party_menu.c
@@ -2766,7 +2766,6 @@ static void CB2_ReturnToPartyMenuFromSummaryScreen(void)
     gPartyMenu.slotId = gLastViewedMonIndex;
     //InitPartyMenu(gPartyMenu.menuType, KEEP_PARTY_LAYOUT, gPartyMenu.action, TRUE, PARTY_MSG_DO_WHAT_WITH_MON, Task_TryCreateSelectionWindow, gPartyMenu.exitCallback);
     InitPartyMenu(gPartyMenu.menuType, KEEP_PARTY_LAYOUT, gPartyMenu.action, TRUE, PARTY_MSG_CHOOSE_MON, Task_HandleChooseMonInput, gPartyMenu.exitCallback);
-    Task_ClosePartyMenu();
 }
 
 static void CursorCb_Switch(u8 taskId)
