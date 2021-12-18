8034c21528d2940f05c67eab2d660dd82c2e2e8c
ydarissep <60859033+ydarissep@users.noreply.github.com>
2021-12-18 21:51:53 +0100

    Update battle_controller_player.c

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -298,7 +298,7 @@ static void HandleInputChooseAction(void)
     }
     else if (JOY_NEW(R_BUTTON))
     {
-        OpenPartyMenuToShowEnemySummaryScreen();
+        //OpenPartyMenuToShowEnemySummaryScreen();
         /*
         switch (gActionSelectionCursor[gActiveBattler])
         {
