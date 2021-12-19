059e1a757d9ff6f8a1b48ac4782d25acb1736c3f
ydarissep <60859033+ydarissep@users.noreply.github.com>
2021-12-19 03:48:39 +0100

    Update battle_controller_player.c

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -311,7 +311,7 @@ static void HandleInputChooseAction(void)
         switch (gActionSelectionCursor[gActiveBattler])
         {
         default:
-            BtlController_EmitTwoReturnValues(1, B_ACTION_SUMMARY, 0);
+            BtlController_EmitTwoReturnValues(1, B_ACTION_SWITCH, 0);
             break;
         }
         PlayerBufferExecCompleted();
