b9bbda81af2cbb0d925b90c5279dcb7e7a37e9f8
ydarissep <60859033+ydarissep@users.noreply.github.com>
2021-12-19 11:50:07 +0100

    Update battle_controller_player.c

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -299,11 +299,11 @@ static void HandleInputChooseAction(void)
     else if (JOY_NEW(R_BUTTON))
     {
         BeginNormalPaletteFade(0xFFFFFFFF, 0, 0, 0x10, RGB_BLACK);
-        //gBattlerInMenuId = gActiveBattler;
+        gBattlerInMenuId = gActiveBattler;
         //gBattlerControllerFuncs[gActiveBattler] = WaitForMonSelection;
         DestroyTask(gBattleControllerData[gActiveBattler]);
         FreeAllWindowBuffers();
-        OpenEnemySummaryScreenMenuInBattle();
+        gBattlerControllerFuncs[gActiveBattler] = OpenEnemySummaryScreenMenuInBattle
         //PlayerHandleShowEnemySummaryScreen();
         /*
         switch (gActionSelectionCursor[gActiveBattler])
