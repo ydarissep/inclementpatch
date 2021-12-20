c026c79f8f76388c40481baa6902c65248c4f8ea
ydarissep <60859033+ydarissep@users.noreply.github.com>
2021-12-20 05:01:05 +0100

    Update battle_controller_player.c

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -1573,10 +1573,14 @@ static void WaitForMonSelection(void)
 {
     if (gMain.callback2 == BattleMainCB2 && !gPaletteFade.active)
     {
+        /*
         if (gPartyMenuUseExitCallback == TRUE)
             BtlController_EmitChosenMonReturnValue(1, gSelectedMonPartyId, gBattlePartyCurrentOrder);
         else
             BtlController_EmitChosenMonReturnValue(1, PARTY_SIZE, NULL);
+        */
+        
+        BtlController_EmitChosenMonReturnValue(1, PARTY_SIZE, NULL);
         
         if ((gBattleResources->bufferA[gActiveBattler][1] & 0xF) == 1)
             PrintLinkStandbyMsg();
