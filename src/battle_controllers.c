946cd64296354b8b778e73de9a708c7536d14708
ydarissep <60859033+ydarissep@users.noreply.github.com>
2021-12-21 03:25:06 +0100

    Update battle_controllers.c

--- src/battle_controllers.c
+++ src/battle_controllers.c
@@ -223,13 +223,13 @@ static void InitSinglePlayerBtlControllers(void)
     {
         gBattleMainFunc = BeginBattleIntro;
 
-        gBattlerControllerFuncs[0] = SetControllerToPlayer;
+        gBattlerControllerFuncs[0] = SetControllerToPlayerPartner;
         gBattlerPositions[0] = B_POSITION_PLAYER_LEFT;
 
         gBattlerControllerFuncs[1] = SetControllerToOpponent;
         gBattlerPositions[1] = B_POSITION_OPPONENT_LEFT;
 
-        gBattlerControllerFuncs[2] = SetControllerToPlayer;
+        gBattlerControllerFuncs[2] = SetControllerToPlayerPartner;
         gBattlerPositions[2] = B_POSITION_PLAYER_RIGHT;
 
         gBattlerControllerFuncs[3] = SetControllerToOpponent;
