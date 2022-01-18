b00e86ccddb55e2b25e66b6dafd8d13d07f1fb4c
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-01-18 15:37:59 +0100

    Update reshow_battle_screen.c

--- src/reshow_battle_screen.c
+++ src/reshow_battle_screen.c
@@ -168,7 +168,6 @@ static void CB2_ReshowBattleScreenAfterMenu(void)
     }
     gSpecialVar_0x8005 = 0;
     gBattleScripting.reshowMainState++;
-    PlayerHandleChooseAction();
 }
 
 static void sub_80A95F4(void)
