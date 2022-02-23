e7e3437245fec6c748e2b3d4798ed9baeaff0b9d
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-02-23 15:41:54 +0100

    Update pokemon.c

--- src/pokemon.c
+++ src/pokemon.c
@@ -8955,7 +8955,6 @@ u16 getRandomSpecies(void)
             altForms = 1;
             
             
-        SeedRngWithRtc();
         Rand = Random() % num;
         if (Rand < numObt)
         {
