78a05ad8dce466a4879db01e9574482654437965
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-03-11 05:48:15 +0100

    Update field_specials.h

--- include/field_specials.h
+++ include/field_specials.h
@@ -33,6 +33,5 @@ void ResetFanClub(void);
 bool8 ShouldShowBoxWasFullMessage(void);
 void SetPCBoxToSendMon(u8 boxId);
 void ResetDailyHiddenItemFlags(void);
-s16 GetBadgeNameFromMoveID (u16 moveID);
 
 #endif // GUARD_FIELD_SPECIALS_H
