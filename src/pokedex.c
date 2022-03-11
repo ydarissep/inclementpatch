c9fc2631b05246bbd245a84a0c5943f7307e09c2
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-03-11 09:51:41 +0100

    Update pokedex.c

--- src/pokedex.c
+++ src/pokedex.c
@@ -6560,25 +6560,25 @@ static void PrintStatsScreen_MoveNameAndInfo(u8 taskId)
         switch (numBadge)
         {
             case 1:
-                PrintInfoScreenTextSmall(gText_dexStone, moves_x + 102, moves_y + 18);
+                PrintInfoScreenTextSmall(gText_dexStone, moves_x + 102, moves_y + 17);
                 break;
             case 2:
-                PrintInfoScreenTextSmall(gText_dexKnuckle, moves_x + 102, moves_y + 18);
+                PrintInfoScreenTextSmall(gText_dexKnuckle, moves_x + 102, moves_y + 17);
                 break;
             case 3:
-                PrintInfoScreenTextSmall(gText_dexDynamo, moves_x + 102, moves_y + 18);
+                PrintInfoScreenTextSmall(gText_dexDynamo, moves_x + 102, moves_y + 17);
                 break;
             case 4:
-                PrintInfoScreenTextSmall(gText_dexHeat, moves_x + 102, moves_y + 18);
+                PrintInfoScreenTextSmall(gText_dexHeat, moves_x + 102, moves_y + 17);
                 break;
             case 5:
-                PrintInfoScreenTextSmall(gText_dexBalance, moves_x + 102, moves_y + 18);
+                PrintInfoScreenTextSmall(gText_dexBalance, moves_x + 102, moves_y + 17);
                 break;
             case 6:
-                PrintInfoScreenTextSmall(gText_dexFeather, moves_x + 102, moves_y + 18);
+                PrintInfoScreenTextSmall(gText_dexFeather, moves_x + 102, moves_y + 17);
                 break;
             case 7:
-                PrintInfoScreenTextSmall(gText_dexMind, moves_x + 102, moves_y + 18);
+                PrintInfoScreenTextSmall(gText_dexMind, moves_x + 102, moves_y + 17);
                 break;
                 /*
             case 8:
@@ -6613,8 +6613,8 @@ static void PrintStatsScreen_MoveNameAndInfo(u8 taskId)
     gTasks[taskId].data[3] = AddItemIconSprite(ITEM_TAG, ITEM_TAG, item);
     if (item == ITEM_TEACHY_TV)
     {
-        gSprites[gTasks[taskId].data[3]].x = 201;
-        gSprites[gTasks[taskId].data[3]].y = 38;
+        gSprites[gTasks[taskId].data[3]].x = 200;
+        gSprites[gTasks[taskId].data[3]].y = 37;
         gSprites[gTasks[taskId].data[3]].oam.priority = 0;
     }
     else
