0cf72b56a6d78ae56710320b0cae9deb267948aa
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-03-15 09:15:02 +0100

    Update wild_encounter.c

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -1920,17 +1920,8 @@ static u16 GetRandomWildEncounterWithBST (u16 species)
         */
         
         //if we do not share both types go to next loop iteration
-        if (gBaseStats[species].type1 == gBaseStats[possibleWildEncounter[i][0]].type1)
-        {
-            if (!gBaseStats[species].type2 == gBaseStats[possibleWildEncounter[i][0]].type2)
-                continue;
-        }
-        else if (gBaseStats[species].type1 == gBaseStats[possibleWildEncounter[i][0]].type2)
-        {
-            if (!gBaseStats[species].type2 == gBaseStats[possibleWildEncounter[i][0]].type1)
-                continue;
-        }
-        else
+        if (!(gBaseStats[species].type1 == gBaseStats[possibleWildEncounter[i][0]].type1 && gBaseStats[species].type2 == gBaseStats[possibleWildEncounter[i][0]].type2)
+        && !(gBaseStats[species].type1 == gBaseStats[possibleWildEncounter[i][0]].type2 && gBaseStats[species].type2 == gBaseStats[possibleWildEncounter[i][0]].type1))
             continue;
         
         // if possibleWildEncounter[i][0] is between desired BST range add it to speciesInBSTRange
