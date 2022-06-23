892be9d858f8eca99921c6091955eb081532afe7
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-06-23 03:21:16 +0200

    Update pokemon.h

--- include/pokemon.h
+++ include/pokemon.h
@@ -457,4 +457,5 @@ u16 GetFormSpeciesId(u16 speciesId, u8 formId);
 u8 GetFormIdFromFormSpeciesId(u16 formSpeciesId);
 u16 GetFormChangeTargetSpecies(struct Pokemon *mon, u16 method, u32 arg);
 u8 GetLevelCap(void);
+void setPersonality(struct BoxPokemon *boxMon, u32 personality);
 #endif // GUARD_POKEMON_H
